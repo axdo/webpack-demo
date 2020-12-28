5b9932e37f8d7a67f5dcc8e97ac013121ad9d62d
cobram <570403541@qq.com>
2020-12-28 10:49:16 +0800

    准备上传了

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,5 +14,47 @@ module.exports = {
             template: 'src/assets/test.html'
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            },
+            {
+                test: /\.styl$/,
+                loader: [ "style-loader" // creates style nodes from JS strings
+                ,"css-loader" // translates CSS into CommonJS
+                , "stylus-loader" // compiles Less to CSS
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('dart-sass')
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
 
